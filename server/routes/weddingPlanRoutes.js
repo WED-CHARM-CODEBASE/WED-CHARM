ff94279c9e44ac93b0207737cfa3405c04d11756
Nitish Kumar <nk9990207902@gmail.com>
2024-10-11 12:11:56 +0530

    changes in routes.

--- server/routes/weddingPlanRoutes.js
+++ server/routes/weddingPlanRoutes.js
@@ -1,10 +1,11 @@
+// imports
 const {generateWeddingPlan} = require("../controllers/weddingPlanController");
 const express =require("express");
-
-
 const router = express.Router();
 
+
+// routes
 router.route("/generate-wedding-plan").post(generateWeddingPlan);
 
-
+// exports
 module.exports = router;
